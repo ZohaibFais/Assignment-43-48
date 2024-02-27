c17044c5f7099c771b3c7b6d525c881d350299d7
ZohaibFais <murtuza.tcc@gmail.com>
2024-02-27 15:34:32 +0500

    Complete

---
+++ index.js
@@ -0,0 +1,15 @@
+// var counter = 0; 
+
+// function updateCounter() {
+//     document.getElementById("counter").textContent = counter; 
+// }
+
+// function increaseCounter() {
+//     counter++;
+//     updateCounter(); 
+// }
+
+// function decreaseCounter() {
+//     counter--;
+//     updateCounter();
+// }
